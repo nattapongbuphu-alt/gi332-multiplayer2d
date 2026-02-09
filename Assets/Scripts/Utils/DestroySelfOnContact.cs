9b3bb80c0e6034bf0d78ea35d0f3a0b18ac0bc07
Nattapong2004 <nattapongbuphu@gmail.com>
2026-02-10 01:13:04 +0700

    Save

---
+++ Assets/Scripts/Utils/DestroySelfOnContact.cs
@@ -0,0 +1,20 @@
+
+using UnityEngine;
+
+
+public class DestroySelfOnContact : MonoBehaviour
+
+{
+
+    private void OnTriggerEnter2D(Collider2D
+collision)
+
+    {
+
+        Destroy(gameObject);
+
+    }
+
+}
+
+
---
+++ Assets/Scripts/Utils/Lifetime.cs
@@ -0,0 +1,20 @@
+
+using UnityEngine;
+
+
+public class Lifetime : MonoBehaviour
+
+{
+
+    [SerializeField] private float lifetime = 1f;
+
+
+    private void Start()
+
+    {
+
+        Destroy(gameObject, lifetime);
+
+    }
+
+}
